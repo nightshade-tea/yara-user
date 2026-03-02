dc6add165a67eb154562c05c280903ffeb6d8ac8
phfr24 <phfr24@inf.ufpr.br>
2026-03-02 17:29:23 -0300

    update

--- main.c
+++ main.c
@@ -3,6 +3,18 @@
 #include <stdarg.h>
 #include <yara.h>
 
+const char *dummy_rule = "\
+rule dummy { \
+  condition: \
+    \"dummy\"\
+}\
+";
+
+const char buf1[] = "sapatopatopato";
+const char buf2[] = "dummybuf";
+
+/* -------------------------------------------------------------------------- */
+
 #define pe(err) \
   error ("(%s:%d) %s() failed: %d\n", __FILE__, __LINE__, __func__, err)
 
@@ -24,12 +36,40 @@ error (const char *restrict format, ...)
   exit (EXIT_FAILURE);
 }
 
+/* -------------------------------------------------------------------------- */
+
+static int
+callback(
+    YR_SCAN_CONTEXT* context,
+    int message,
+    void* message_data,
+    void* user_data)
+{
+  printf ("message=%d\n", message);
+
+  return CALLBACK_CONTINUE;
+}
+
 int main ()
 {
+  YR_COMPILER *compiler;
+  YR_RULES *rules;
   int err;
 
   chk (yr_initialize ());
-  chk (yr_finalize ());
+  chk (yr_compiler_create (&compiler));
+
+  chk (yr_compiler_add_string (compiler, dummy_rule, NULL));
+  chk (yr_compiler_get_rules (compiler, &rules));
+
+  puts ("buf2");
+  chk (yr_rules_scan_mem (rules, buf2, sizeof buf2, 0, callback, 0, 0));
+  puts ("buf1");
+  chk (yr_rules_scan_mem (rules, buf1, sizeof buf1, 0, callback, 0, 0));
+
+  yr_rules_destroy (rules);
+  yr_compiler_destroy (compiler);
+  yr_finalize ();
 
   return EXIT_SUCCESS;
 }
